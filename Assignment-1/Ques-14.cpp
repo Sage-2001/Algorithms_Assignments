9f221a4e542b53f22c4c8964870c93ed84044584
Sage_2001 <somya.malgudi@gmail.com>
2023-08-05 16:21:42 +0530

    Done 14 and 15

---
+++ Assignment-1/Ques-14.cpp
@@ -0,0 +1,27 @@
+// 14. Given a sequence of n numbers, representing the stock prices of a stock on different
+// days, design a linear time algorithm to compute the maximum profit that you can
+// make by buying and selling a stock exactly once, you can sell a stock only after you
+// buy it.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
---
+++ Assignment-1/Ques-15.cpp
@@ -0,0 +1,28 @@
+// 15. Given a sequence of n numbers, representing the stock prices of a stock on different
+// days, design a linear time algorithm to compute the maximum profit that you can
+// make by buying and selling a stock exactly once, you can sell a stock exactly k days
+// after you bought it.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr,int k) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < n - k; i++) {
+        minPrice = min(minPrice, arr[i+k]);
+        maxPro = max(maxPro, arr[i] - minPrice);
+    }
+    
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int k = 2;
+    int maxPro = maxProfit(arr,k);
+    cout << "Max profit is: " << maxPro << endl;
+}
